33e6ab2f6738c1cce35c63a35847318624a59d21
Fahim Anzam Dip <dfahimanzam@gmail.com>
2020-10-21 01:42:27 +0600

    Tweaks

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -12,7 +12,7 @@ class EmployeesController extends Controller
 {
 
     public function index() {
-        $title = "Dashboard - Employees";
+        $title = "Dashboard - Friends";
         $employees = Employee::latest()->get();
 
         return view('pages.employees.index', [
@@ -23,7 +23,7 @@ class EmployeesController extends Controller
 
 
     public function create() {
-        $title = "Dashboard - Create Employee";
+        $title = "Dashboard - Create Friend";
 
         return view('pages.employees.create', [
             'title' => $title
@@ -53,7 +53,7 @@ class EmployeesController extends Controller
 
 
     public function edit($id) {
-        $title = "Dashboard - Edit Employee";
+        $title = "Dashboard - Edit Friend";
         $employee = Employee::find($id);
 
         return view('pages.employees.edit', [
