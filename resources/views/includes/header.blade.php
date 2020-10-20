be78561a77edb67cdb099ed4f7057d12a3b3de7f
Fahim Anzam Dip <dfahimanzam@gmail.com>
2020-10-20 21:13:45 +0600

    Tweaks

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <header class="app-header">
-    <a class="app-header__logo" href="{{ url('/') }}">Tiptop Plus</a>
+    <a class="app-header__logo" href="{{ url('/') }}">Wisher</a>
     <!-- Sidebar toggle button-->
     <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <!-- Navbar Right Menu-->
